1eb68547d01822bbe1d5662f9deda66d83f8b993
Vinoth Subramanian <Vinoth.Subramanian@cqc.org.uk>
2019-12-29 21:05:03 +0000

    EncryptandDecrypt

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EncryptionDecryptionUsingSymmetricKey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Guid key generated with base64encoded and uppercase combination 
+            var key = "A+W2nzdpbEe3UHrCBZU5Qw==";                       
+
+            //Console.WriteLine("Please enter a secret key for the symmetric algorithm.");  
+            //var key = Console.ReadLine();  
+
+            Console.WriteLine("Please enter a string for encryption");
+            var str = Console.ReadLine();
+            var encryptedString = AesAlgorithm.Encrypt(key, str);
+            Console.WriteLine($"encrypted string = {encryptedString}");
+
+            var decryptedString = AesAlgorithm.Decrypt(key, encryptedString);
+            Console.WriteLine($"decrypted string = {decryptedString}");
+
+            Console.ReadKey();
+        }
+    }
+}
